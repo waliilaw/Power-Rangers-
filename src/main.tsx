3706d2449d89b9010487dcada2544077d5c93940
Wali <humaidwali20@gmail.com>
2024-11-15 01:35:02 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ createRoot(document.getElementById('root')!).render(
 <KindeProvider
 		clientId="d0649206c4bc405a9eae7e083e2eafba"
 		domain="https://rangers.kinde.com"
-		redirectUri="https://redranger.vercel.app/1"
-		logoutUri="https://redranger.vercel.app/1"
+		redirectUri="https://redranger.vercel.app"
+		logoutUri="https://redranger.vercel.app"
 	>
 		<App />
 	</KindeProvider>
