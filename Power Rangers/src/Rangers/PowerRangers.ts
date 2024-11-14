b600c43910a8d89fc725c70f77e83a02a3167cd6
Wali <humaidwali20@gmail.com>
2024-11-14 19:12:04 +0530

    Delete Power Rangers directory

--- Power Rangers/src/Rangers/PowerRangers.ts
+++
@@ -1,5 +0,0 @@
-export const PowerRangers = [
-    { name: "Red Ranger", color: "red", image: "/path/to/red-ranger.jpg" },
-    { name: "Blue Ranger", color: "blue", image: "/path/to/blue-ranger.jpg" },
-    { name: "Green Ranger", color: "green", image: "/path/to/green-ranger.jpg" },
-]
