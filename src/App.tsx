3d13d36594db6680819167620d1eb6e9c0ae1308
Wali <humaidwali20@gmail.com>
2024-11-15 01:07:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import First from './pages/First'
 import Second from "./pages/Second";
-import Third from "./pages/Third";
 import { Navigate } from "react-router-dom";
 import './App.css'
 
@@ -12,10 +11,10 @@ return(<>
 <Router>
 <div>
 <Routes>
+    
 <Route path="/" element={<Navigate to="/1" replace />} />
 <Route path = "/1" element={<First />} />
 <Route path = "/2" element={<Second />}/>
-<Route path = "/3" element={<Third />} />
 
 </Routes>
 
