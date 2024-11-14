0d936b41d644f531eb710042d54de14eaa3717b6
Wali <humaidwali20@gmail.com>
2024-11-14 19:20:21 +0530

    Update First.tsx

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -2,12 +2,13 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { useNavigate } from "react-router-dom";
 
 function First() {
-  const { login, register, isAuthenticated, user } = useKindeAuth();
+  const { login, register, isAuthenticated, user , logout } = useKindeAuth();
 
 
   const handleLogin = () => login();
   const handleRegister = () => register();
   const navigate = useNavigate()
+  const handleLogout = () => logout()
 
   function Home(){
     navigate("/2")
@@ -19,6 +20,7 @@ function First() {
           <h2>Welcome </h2>
           <p>You are logged in.</p>
           <button onClick={Home}>Go to Home </button>
+          <button onClick={handleLogout}>Log out</button>
         </div>
       ) : (
         <div>
