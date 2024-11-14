fa8f89ec613fcd1c6306b7a9ad55439f417948bf
Wali <humaidwali20@gmail.com>
2024-11-14 21:21:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <KindeProvider
-  clientId="d0649206c4bc405a9eae7e083e2eafba"
-  domain="https://rangers.kinde.com"
-  redirectUri="http://localhost:3000"
-  logoutUri="http://localhost:3000"
->
-  <App />
-</KindeProvider>
+<KindeProvider
+		clientId="d0649206c4bc405a9eae7e083e2eafba"
+		domain="https://rangers.kinde.com"
+		redirectUri="https://rangers-zeta.vercel.app/1"
+		logoutUri="https://rangers-zeta.vercel.app/1"
+	>
+		<App />
+	</KindeProvider>
 )
