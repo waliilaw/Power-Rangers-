44e287ccb681995ffc87113c62852b06b1d1e37a
Wali <humaidwali20@gmail.com>
2024-11-15 16:13:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import First from './pages/First'
+import First from './pages/First';
 import Second from "./pages/Second";
 import './App.css'
 
-function App(){
-
-
-return(<>
-<Router>
-<div>
-<Routes>
-    
-<Route path = "/" element={<First />} />
-<Route path = "/2" element={<Second />}/>
-
-</Routes>
-
-</div>
-</Router>
-</>)
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+    <Routes>
+            <Route path="/" element={<First />} />
+            <Route path="/2" element={<Second />} />
+    </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
