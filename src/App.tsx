104aa989b1d7c3e09c2b57e4400d89d019fa16d1
Wali <humaidwali20@gmail.com>
2024-11-16 17:07:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import First from './pages/First';
 import Second from "./pages/Second";
 import './App.css'
+import {Feedback} from './pages/Feedback';
+import {CreaterInfo} from './pages/CreaterInfo';
+import {YourRanger} from './pages/YourRanger';
 
 function App() {
   return (
     <>
+    <Feedback/>
+    <CreaterInfo/>
+    <YourRanger/>
       <Router>
         <div>
     <Routes>
