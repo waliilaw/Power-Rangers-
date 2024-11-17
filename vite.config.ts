3d97bbc5141c687a5bb1c10cbb088a86f4926cae
Wali <humaidwali20@gmail.com>
2024-11-17 11:55:06 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 3000,  // Set the port to 3000
+    port : 3000
   },
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
