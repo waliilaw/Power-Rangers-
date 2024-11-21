810dcd9a8462350dbdf486904aae8ded4a9d2b21
Wali <humaidwali20@gmail.com>
2024-11-21 17:55:07 +0530

    Delete src directory

--- src/logic/hash.ts
+++
@@ -1,7 +0,0 @@
-export function hashUsername(username: string): number{
-    let hash = 0;
-    for (let i = 0; i < username.length; i++) {
-      hash = username.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return Math.abs(hash);
-  };
