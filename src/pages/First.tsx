8eeaa7f0fcef462084588cb7740fbf10bb92c582
Wali <humaidwali20@gmail.com>
2024-11-22 20:03:52 +0530

    Add files via upload

---
+++ src/pages/First.tsx
@@ -0,0 +1,50 @@
+import './First.css';
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import { useState } from "react";
+import { Second } from './Second';
+import Three from './Three';
+
+function First() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { login, logout } = useKindeAuth();
+
+  async function Login() {
+    setIsLoading(true);
+    try {
+      await login();
+    } catch (error) {
+      console.error("Login failed", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function Logout() {
+    setIsLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    try {
+      await logout();
+    } catch (error) {
+      console.error("Logout failed", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <img src="/asta.gif" alt="Loading..." />
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <Three />
+      <Second onLogin={Login} onLogout={Logout} />
+    </div>
+  );
+}
+
+export default First;
