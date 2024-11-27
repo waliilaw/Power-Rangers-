2a404aaa98016e1d2cd67253a69d8f70979b6d06
Wali <humaidwali20@gmail.com>
2024-11-28 03:35:14 +0530

    Update First.tsx

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -1,36 +1,36 @@
 import './First.css';
-// import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { useState } from "react";
 import Tryi from './Front/try';
-// import { Second } from './Second';
+import { Second } from './Second';
 // import Three from './Three';
 
 function First() {
-  const [isLoading, ] = useState(false);
-  // const { login, logout } = useKindeAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const { login, logout } = useKindeAuth();
 
-  // async function Login() {
-  //   setIsLoading(true);
-  //   try {
-  //     await login();
-  //   } catch (error) {
-  //     console.error("Login failed", error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
+  async function Login() {
+    setIsLoading(true);
+    try {
+      await login();
+    } catch (error) {
+      console.error("Login failed", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
-  // async function Logout() {
-  //   setIsLoading(true);
-  //   await new Promise((resolve) => setTimeout(resolve, 1000));
-  //   try {
-  //     await logout();
-  //   } catch (error) {
-  //     console.error("Logout failed", error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
+  async function Logout() {
+    setIsLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    try {
+      await logout();
+    } catch (error) {
+      console.error("Logout failed", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   if (isLoading) {
     return (
@@ -44,7 +44,7 @@ function First() {
     <div className="container">
       <Tryi />
       {/* <Three /> */}
-      {/* <Second onLogin={Login} onLogout={Logout} /> */}
+      <Second onLogin={Login} onLogout={Logout} />
     </div>
   );
 }
