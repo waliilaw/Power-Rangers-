e254f7256be3f5ee36d01571b6baca80f2f1adfb
Wali <humaidwali20@gmail.com>
2024-11-28 03:06:49 +0530

    Add files via upload

---
+++ src/pages/pages/Second.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import SeeYourRanger from "@/components/ui/SeeYourRanger";
+
+interface SecondProps {
+  onLogin: () => void;
+  onLogout: () => void;
+}
+
+export function Second({ onLogin, onLogout }: SecondProps): JSX.Element {
+  const { isAuthenticated } = useKindeAuth();
+  const [, setDrawerOpen] = useState(false);
+
+  return (
+    <>
+      <div className="button-container flex flex-col items-center space-y-4">
+        {isAuthenticated ? (
+          <div>
+            <h1></h1>
+            <button onClick={onLogout} className="button logout-btn">
+              Logout
+            </button>
+            <button
+        onClick={() => setDrawerOpen(true)}
+        className="button open-drawer"
+      >
+        Open Drawer
+      </button>
+            <SeeYourRanger />
+          </div>
+        ) : (
+          <div>
+
+            <button onClick={onLogin} className="button login-btn">
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+
+
+    </>
+  );
+}
