7827f9b4794286835f986248a61a2e7d46d5e8ea
Wali <humaidwali20@gmail.com>
2024-12-03 14:14:54 +0530

    Update First.tsx

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -7,7 +7,7 @@ import { Footer } from './Feet/Footer';
 // import Three from './Three';
 
 function First() {
-  const { login, logout, user } = useKindeAuth();
+  const { login, logout, user , isAuthenticated } = useKindeAuth();
   const [rangerImage, setRangerImage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -96,29 +96,29 @@ if (isLoading) {
         <Second onLogin={Login} onLogout={Logout} />
       )}
 
-      {/* Background */}
-      <div className="content">
-        <div className="up">
-          <span className="up-0">R</span>
-          <span className="up-2-8">A</span>
-          <span className="up-3-7">N</span>
-          <span className="up-4-6">G</span>
-          <span className="up-5">E</span>
-          <span className="up-4-6">R</span>
-          <span className="up-3-7">S</span>
+      {!isAuthenticated && (
+        <div className="content">
+          <div className="up">
+            <span className="up-0">R</span>
+            <span className="up-2-8">A</span>
+            <span className="up-3-7">N</span>
+            <span className="up-4-6">G</span>
+            <span className="up-5">E</span>
+            <span className="up-4-6">R</span>
+            <span className="up-3-7">S</span>
+          </div>
+          <div className="down">
+            <span className="down-0">R</span>
+            <span className="down-2-8">A</span>
+            <span className="down-3-7">N</span>
+            <span className="down-4-6">G</span>
+            <span className="down-5">E</span>
+            <span className="down-4-6">R</span>
+            <span className="down-3-7">S</span>
+          </div>
         </div>
-        <div className="down">
-          <span className="down-0">R</span>
-          <span className="down-2-8">A</span>
-          <span className="down-3-7">N</span>
-          <span className="down-4-6">G</span>
-          <span className="down-5">E</span>
-          <span className="down-4-6">R</span>
-          <span className="down-3-7">S</span>
-        </div>
-      </div>
+      )}
     </>
-  )
-}
+  )}
 
 export default First
