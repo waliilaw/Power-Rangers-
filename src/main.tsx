14f0ea81b151b6f9f6dc7bd0219e7a21226aab09
Wali <humaidwali20@gmail.com>
2024-12-06 16:08:08 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import {KindeProvider} from "@kinde-oss/kinde-auth-react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from "react";
 
 createRoot(document.getElementById('root')!).render(
-<KindeProvider
-		clientId="d0649206c4bc405a9eae7e083e2eafba"
-		domain="https://rangers.kinde.com"
-		redirectUri="https://redranger.vercel.app"
-		logoutUri="https://redranger.vercel.app"
+	<KindeProvider
+		clientId="9e2def827e5a47e1938524b6bd9732bd"
+		domain="https://testforrangers.kinde.com"
+		redirectUri="http://localhost:3000"
+		logoutUri="http://localhost:3000"
 	>
 		<App />
 	</KindeProvider>
