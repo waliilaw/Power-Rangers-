124c5c475898023596fb6dcc3c4eeed463f6ba02
Wali <humaidwali20@gmail.com>
2024-12-22 21:03:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import {KindeProvider} from "@kinde-oss/kinde-auth-react";
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 import React from "react";
 
 createRoot(document.getElementById('root')!).render(
 	<KindeProvider
-		clientId=""
-		domain=""
-		redirectUri=""
-		logoutUri=""
+		clientId={import.meta.env.VITE_CLIENT_ID}
+		domain={import.meta.env.VITE_DOMAIN}
+		redirectUri={import.meta.env.VITE_REDIRECT_URI}
+		logoutUri={import.meta.env.VITE_LOGOUT_URI}
 	>
 		<App />
 	</KindeProvider>
-)
+);
